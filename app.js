dfd0e67b7e59c25288e1ed6484f4174f425d5f77
Robert <tre8borrobert@yandex.ru>
2022-07-11 23:55:02 +0300

    update app.js #1

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log('Some text');
 console.log('Some text 2');
+
+window.addEventListener('DOMContentLoaded', () => {
+	const timer = setTimeout(() => {
+		console.log('setTimeout');
+		clearTimeout(timer);
+	}, 5000);
+});
+
